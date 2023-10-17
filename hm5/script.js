f37a321ebc0b04e1caaf38747ae4a65ba3b505a9
voin12K <vladleurda@gmail.com>
2023-10-17 21:13:55 +0300

    hm5

--- hm5/script.js
+++ hm5/script.js
@@ -1,47 +1,55 @@
-console.log("Welcome to calculator!")
-const userOperation = prompt("What action you want to do? Add, Diff, Mult, Div, Sqrt, Sin, Cos");
+console.log("Welcome to the calculator!");
+let userOperation = prompt("What action do you want to perform? (Add, Diff, Mult, Div, Sqrt, Sin, Cos)");
 
 if (userOperation === null) {
-    console.log("Good by, see you later.");
+    console.log("Goodbye, see you later.");
 } else {
+    userOperation = userOperation.toLowerCase(); // Convert userOperation to lowercase for case insensitivity
+
     if (userOperation === "add" || userOperation === "diff" || userOperation === "mult" || userOperation === "div") {
-        const userNumber1 = parseFloat(prompt("first number:"));
-        const userNumber2 = parseFloat(prompt("first number::"));
+        const userNumber1 = parseFloat(prompt("Enter the first number:"));
 
-        if (!isNaN(userNumber1) && !isNaN(userNumber2)){
-            if (userOperation === "add") {
-                console.log("sum: " + userNumber1 + " + " + userNumber2 + " = " + (userNumber1 + userNumber2));
+        if (!isNaN(userNumber1)) {
+            const userNumber2 = parseFloat(prompt("Enter the second number:"));
+            if (!isNaN(userNumber2)) {
+                if (userOperation === "add") {
+                    console.log("sum: " + userNumber1 + " + " + userNumber2 + " = " + (userNumber1 + userNumber2));
+                } else if (userOperation === "diff") {
+                    console.log("diff: " + userNumber1 + " - " + userNumber2 + " = " + (userNumber1 - userNumber2));
+                } else if (userOperation === "mult") {
+                    console.log("mult: " + userNumber1 + " * " + userNumber2 + " = " + (userNumber1 * userNumber2));
+                } else if (userOperation === "div") {
+                    if (userNumber2 === 0) {
+                        console.log("Division by zero is not allowed.");
+                    } else {
+                        console.log("div: " + userNumber1 + " / " + userNumber2 + " = " + (userNumber1 / userNumber2));
+                    }
+                }
+            } else {
+                console.log("The second entered number is not valid. Goodbye.");
             }
-            if (userOperation === "diff") {
-                console.log("diff: " + userNumber1 + " - " + userNumber2 + " = " + (userNumber1 - userNumber2));
-            }
-            if (userOperation === "mult") {
-                console.log("mult: " + userNumber1 + " * " + userNumber2 + " = " + (userNumber1 * userNumber2));
-            }
-            if (userOperation === "div") {
-                console.log("div: " + userNumber1 + " / " + userNumber2 + " = " + (userNumber1 / userNumber2));
-            }
-
-            console.log("Good by, see you later.")
         } else {
-            console.log("This is bad digit, good by")
+            console.log("The first entered number is not valid. Goodbye.");
         }
-    } else {
-        const userNumber = parseFloat(prompt("number:"));
+    } else if (userOperation === "sqrt" || userOperation === "sin" || userOperation === "cos") {
+        const userNumber = parseFloat(prompt("Enter a number:"));
+
         if (!isNaN(userNumber)) {
-            if (userOperation === "sqrt"){
-                console.log("sqrt " + userNumber + " = " + Math.sqrt(userNumber));
-            }
-            if (userOperation === "sin"){
+            if (userOperation === "sqrt") {
+                if (userNumber < 0) {
+                    console.log("Square root of a negative number is not allowed.");
+                } else {
+                    console.log("sqrt " + userNumber + " = " + Math.sqrt(userNumber));
+                }
+            } else if (userOperation === "sin") {
                 console.log("sin " + userNumber + " = " + Math.sin(userNumber));
-            }
-            if (userOperation === "cos"){
+            } else if (userOperation === "cos") {
                 console.log("cos " + userNumber + " = " + Math.cos(userNumber));
             }
-
-            console.log("Good by, see you later.")
         } else {
-            console.log("This is bad digit, good by")
+            console.log("The entered number is not valid. Goodbye.");
         }
+    } else {
+        console.log("Invalid operation. Goodbye!");
     }
 }
