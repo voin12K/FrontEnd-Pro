f910442f08ef7497ffd259bf8f9e3333b042b203
voin12K <vladleurda@gmail.com>
2023-10-25 23:22:28 +0300

    hm7

--- hm7/script.js
+++ hm7/script.js
@@ -1,62 +1,109 @@
-console.log("Welcome to the calculator!");
-let userContinue = 1;
-while (userContinue === 1 ) {
+alert("Welcome to the calculator!");
+
+let userContinue = true;
+
+while (userContinue) {
     let userOperation = prompt("What action do you want to perform? (add, diff, mult, div, sqrt, sin, cos)");
 
-    while (userOperation !== 'add' && userOperation !== 'diff' && userOperation !== 'mult' && userOperation !== 'div' && userOperation !== 'spqr' && userOperation !== 'sin' && userOperation !== 'cos') {
-        if (userOperation === null) {
-            break;
-        }
-        userOperation = prompt("I don't recognize your operation. Please choose correct operation like:  (add, diff, mult, div, sqrt, sin, cos)");
-    }
-
-    switch (userOperation) {
-        case "add":
-        case "diff":
-        case "mult":
-        case "div":
-            let userNumber1 = parseFloat(prompt("Enter the first number:"));
-            while (isNaN(userNumber1)) {
-                userNumber1 = parseFloat(prompt("This is bad digit. Please enter correct digit"));
-            }
-
-            let userNumber2 = parseFloat(prompt("Enter the first number:"));
-            while (isNaN(userNumber2)) {
-                userNumber2 = parseFloat(prompt("This is bad digit. Please enter correct digit"));
-            }
-            if (!isNaN(userNumber2)) {
-                const operationSymbol = userOperation === "add" ? "+" :
-                    userOperation === "diff" ? "-" :
-                        userOperation === "mult" ? "*" :
-                            "/";
-                const result = userOperation === "add" ? userNumber1 + userNumber2 :
-                    userOperation === "diff" ? userNumber1 - userNumber2 :
-                        userOperation === "mult" ? userNumber1 * userNumber2 :
-                            userNumber2 === 0 ? "Division by zero is not allowed." : userNumber1 / userNumber2;
-                console.log(`${userOperation}: ${userNumber1} ${operationSymbol} ${userNumber2} = ${result}`);
-            }
-            break;
-        case "sqrt":
-        case "sin":
-        case "cos":
-            let userNumber = parseFloat(prompt("Enter a number:"));
-            while (isNaN(userNumber)) {
-                userNumber = parseFloat(prompt("This is bad digit. Please enter correct digit"));
-            }
-            if (!isNaN(userNumber)) {
-                const result = userOperation === "sqrt" ? (userNumber < 0 ? "Square root of a negative number is not allowed." : Math.sqrt(userNumber)) :
-                    userOperation === "sin" ? Math.sin(userNumber) :
-                        Math.cos(userNumber);
-                console.log(`${userOperation} ${userNumber} = ${result}`);
-            } else {
-                console.log("The entered number is not valid. Goodbye.");
-            }
-            break;
-    }
-    userAsk = prompt("Do you want continue work with me? yes/no");
-    if (userAsk === "yes"){
+    if (userOperation === null) {
+        alert('Goodbye, see you later.');
+        userContinue = false;
+    } else if (userOperation !== 'add' && userOperation !== 'diff' && userOperation !== 'mult' && userOperation !== 'div' && userOperation !== 'sqrt' && userOperation !== 'sin' && userOperation !== 'cos') {
+        alert("I don't recognize your operation. Please choose a correct operation like: (add, diff, mult, div, sqrt, sin, cos)");
     } else {
-        console.log( 'Good by, see you later.');
-        break;
+        switch (userOperation) {
+            case "add":
+            case "diff":
+            case "mult":
+            case "div":
+                let userNumber1 = parseFloat(prompt("Enter the first number:"));
+
+                if (userNumber1 === null) {
+                    alert('Goodbye, see you later.');
+                    userContinue = false;
+                    break;
+                }
+
+                while (isNaN(userNumber1)) {
+                    userNumber1 = parseFloat(prompt("This is a bad digit. Please enter a correct digit."));
+                }
+
+                let userNumber2 = parseFloat(prompt("Enter the second number:"));
+
+                if (userNumber2 === null) {
+                    alert('Goodbye, see you later.');
+                    userContinue = false;
+                    break;
+                }
+
+                while (isNaN(userNumber2)) {
+                    userNumber2 = parseFloat(prompt("This is a bad digit. Please enter a correct digit."));
+                }
+
+                if (!isNaN(userNumber2)) {
+                    let result;
+                    let operationSymbol;
+
+                    if (userOperation === "add") {
+                        operationSymbol = "+";
+                        result = userNumber1 + userNumber2;
+                    } else if (userOperation === "diff") {
+                        operationSymbol = "-";
+                        result = userNumber1 - userNumber2;
+                    } else if (userOperation === "mult") {
+                        operationSymbol = "*";
+                        result = userNumber1 * userNumber2;
+                    } else if (userNumber2 !== 0) {
+                        operationSymbol = "/";
+                        result = userNumber1 / userNumber2;
+                    } else {
+                        result = "Division by zero is not allowed.";
+                    }
+
+                    alert(`${userOperation}: ${userNumber1} ${operationSymbol} ${userNumber2} = ${result}`);
+                }
+                break;
+
+            case "sqrt":
+            case "sin":
+            case "cos":
+                let userNumber = parseFloat(prompt("Enter a number:"));
+
+                if (userNumber === null) {
+                    alert('Goodbye, see you later.');
+                    userContinue = false;
+                    break;
+                }
+
+                while (isNaN(userNumber)) {
+                    userNumber = parseFloat(prompt("This is a bad digit. Please enter a correct digit."));
+                }
+
+                if (!isNaN(userNumber)) {
+                    let result;
+
+                    if (userOperation === "sqrt") {
+                        result = userNumber < 0 ? "Square root of a negative number is not allowed." : Math.sqrt(userNumber);
+                    } else if (userOperation === "sin") {
+                        result = Math.sin(userNumber);
+                    } else {
+                        result = Math.cos(userNumber);
+                    }
+
+                    alert(`${userOperation} ${userNumber} = ${result}`);
+                } else {
+                    alert("The entered number is not valid. Goodbye.");
+                }
+                break;
+        }
+    }
+
+    if (userContinue) {
+        let userAsk = prompt("Do you want to continue working with me? (yes/no)");
+
+        if (userAsk === null || userAsk.toLowerCase() !== "yes") {
+            alert('Goodbye, see you later.');
+            userContinue = false;
+        }
     }
 }
