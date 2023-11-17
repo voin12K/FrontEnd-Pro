dd30df7e4c5d3f78fecda18a5eca5a564122d42f
voin12K <vladleurda@gmail.com>
2023-11-17 20:06:02 +0200

    1

--- hm16/script.js
+++ hm16/script.js
@@ -1,32 +1,40 @@
-function Human(name, age) {
-    this.name = name;
-    this.age = age;
+class Human {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+class Avto {
+    constructor(brand, model, year, number) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.number = number;
+        this.owner = null;
+    }
+
+    setOwner(person) {
+        if (person instanceof Human) {
+            if (person.age < 18) {
+                console.log(`Sorry, ${person.name} is not eligible to own a car.`);
+            } else {
+                this.owner = person;
+                console.log(`${person.name} is now the owner of the ${this.brand} ${this.model}.`);
+            }
+        } else {
+            console.log(`Invalid owner. Please provide an instance of the Human class.`);
+        }
+    }
+
+    getOwner() {
+        return this.owner;
+    }
 }
 
 let vlad = new Human("Vlad", 17);
 let nikita = new Human("Nikita", 18);
 let dima = new Human("Dima", 28);
-
-function Avto(brand, model, year, number) {
-    this.brand = brand;
-    this.model = model;
-    this.year = year;
-    this.number = number;
-    this.owner = null;
-
-    this.setOwner = function (person) {
-        if (person.age < 18) {
-            console.log(`Sorry, ${person.name} is not eligible to own a car.`);
-        } else {
-            this.owner = person;
-            console.log(`${person.name} is now the owner of the ${this.brand} ${this.model}.`);
-        }
-    };
-
-    this.getOwner = function () {
-        return this.owner;
-    };
-}
 
 let honda = new Avto("Honda", "Civic", 2000, 1234);
 let bmw = new Avto("BMW", "X5", 2005, 1122);
