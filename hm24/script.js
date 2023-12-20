62a21f5ca446b7bcab5f86814df98135e176aba7
voin12K <vladleurda@gmail.com>
2023-12-20 18:58:26 +0200

    1

--- hm24/script.js
+++ hm24/script.js
@@ -27,29 +27,136 @@ function showProductInfo(product) {
     const productInfoBlock = document.getElementById('product-info');
     clearElement(productInfoBlock);
 
-    const h2 = document.createElement('h2');
-    h2.textContent = product.name;
+    const orderForm = createOrderForm(product);
+    productInfoBlock.appendChild(orderForm);
 
-    const p1 = document.createElement('p');
-    p1.textContent = product.info;
-
-    const p2 = document.createElement('p');
-    p2.textContent = `Ціна: ${product.price}$`;
-
-    const button = document.createElement('button');
-    button.textContent = 'Купити';
-    button.addEventListener('click', buyProduct);
-
-    productInfoBlock.appendChild(h2);
-    productInfoBlock.appendChild(p1);
-    productInfoBlock.appendChild(p2);
-    productInfoBlock.appendChild(button);
+    // Очищення блоку інформації про замовлення
+    clearOrderInfo();
 }
 
-function buyProduct() {
-    alert('Товар куплений!');
-    const productInfoBlock = document.getElementById('product-info');
-    clearElement(productInfoBlock);
+function createOrderForm(product) {
+    const form = document.createElement('form');
+    form.id = 'order-form';
+
+    const pibInput = createInput('text', 'pib', 'ПІБ покупця');
+    const cityInput = createSelect('city', 'Місто', ['Київ', 'Львів', 'Одеса', 'Харків']);
+    const novaPoshtaInput = createInput('text', 'novaPoshta', 'Склад Нової пошти для надсилання');
+    const paymentInput = createRadioGroup('payment', ['Післяплата', 'Оплата банківською карткою']);
+    const quantityInput = createInput('number', 'quantity', 'Кількість продукції, що купується', 1);
+    const commentInput = createInput('text', 'comment', 'Коментар до замовлення');
+
+    const submitButton = document.createElement('button');
+    submitButton.type = 'button';
+    submitButton.textContent = 'Підтвердити замовлення';
+    submitButton.addEventListener('click', () => confirmOrder(product));
+
+    form.appendChild(pibInput);
+    form.appendChild(cityInput);
+    form.appendChild(novaPoshtaInput);
+    form.appendChild(paymentInput);
+    form.appendChild(quantityInput);
+    form.appendChild(commentInput);
+    form.appendChild(submitButton);
+
+    return form;
+}
+
+function confirmOrder(product) {
+    const pib = document.getElementById('pib').value;
+    const city = document.getElementById('city').value;
+    const novaPoshta = document.getElementById('novaPoshta').value;
+    const payment = document.querySelector('input[name="payment"]:checked');
+    const quantity = document.getElementById('quantity').value;
+    const comment = document.getElementById('comment').value;
+
+    // Перевірка на обов'язкові поля
+    if (!pib || !city || !novaPoshta || !payment || !quantity) {
+        alert('Будь ласка, заповніть всі обов\'язкові поля.');
+        return;
+    }
+
+    // Виведення інформації про замовлення
+    const orderInfoBlock = document.getElementById('order-info');
+    clearElement(orderInfoBlock);
+
+    const orderInfo = document.createElement('div');
+    orderInfo.innerHTML = `
+        <h2>Інформація про замовлення</h2>
+        <p><strong>Товар:</strong> ${product.name}</p>
+        <p><strong>ПІБ покупця:</strong> ${pib}</p>
+        <p><strong>Місто:</strong> ${city}</p>
+        <p><strong>Склад Нової пошти:</strong> ${novaPoshta}</p>
+        <p><strong>Спосіб оплати:</strong> ${payment.value}</p>
+        <p><strong>Кількість:</strong> ${quantity}</p>
+        <p><strong>Коментар:</strong> ${comment}</p>
+    `;
+
+    orderInfoBlock.appendChild(orderInfo);
+}
+
+function createInput(type, id, label, defaultValue) {
+    const div = document.createElement('div');
+
+    const input = document.createElement('input');
+    input.type = type;
+    input.id = id;
+    input.required = true;
+    if (defaultValue) {
+        input.value = defaultValue;
+    }
+
+    const labelElement = document.createElement('label');
+    labelElement.textContent = label;
+    labelElement.setAttribute('for', id);
+
+    div.appendChild(labelElement);
+    div.appendChild(input);
+
+    return div;
+}
+
+function createSelect(id, label, options) {
+    const div = document.createElement('div');
+
+    const select = document.createElement('select');
+    select.id = id;
+    select.required = true;
+
+    options.forEach(optionText => {
+        const option = document.createElement('option');
+        option.value = optionText;
+        option.text = optionText;
+        select.appendChild(option);
+    });
+
+    const labelElement = document.createElement('label');
+    labelElement.textContent = label;
+    labelElement.setAttribute('for', id);
+
+    div.appendChild(labelElement);
+    div.appendChild(select);
+
+    return div;
+}
+
+function createRadioGroup(name, options) {
+    const div = document.createElement('div');
+
+    options.forEach(optionText => {
+        const input = document.createElement('input');
+        input.type = 'radio';
+        input.name = name;
+        input.value = optionText;
+        input.required = true;
+
+        const labelElement = document.createElement('label');
+        labelElement.textContent = optionText;
+
+        div.appendChild(input);
+        div.appendChild(labelElement);
+    });
+
+    return div;
 }
 
 function clearElement(element) {
